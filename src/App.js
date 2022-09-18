f65ef6885b700fafee3f7434f9a4c80a0b4b1180
Huan <Huanvo95@gmail.com>
2022-09-18 13:48:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import BaiLayout from './compomemt/BaiLayout/BaiLayout';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+        <BaiLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compomemt/Body/Item/Item.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+export default class Item extends Component {
+    render() {
+        return (
+            <div class="pt-4">
+                <div class="container px-lg-5">
+
+                    <div class="row gx-lg-5">
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                    <i class="fa-solid fa-wifi"></i></div>
+                                    <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                                    <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i class="fa-solid fa-cloud-arrow-down"></i></div>
+                                    <h2 class="fs-4 fw-bold">Free to download</h2>
+                                    <p class="mb-0">As always, Start Bootstrap has a powerful collectin of free templates.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i class="fa-solid fa-credit-card"></i></div>
+                                    <h2 class="fs-4 fw-bold">Jumbotron hero header</h2>
+                                    <p class="mb-0">The heroic part of this template is the jumbotron hero header!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                    <i class="fa-solid fa-b"></i></div>
+                                    <h2 class="fs-4 fw-bold">healer boxes</h2>
+                                    <p class="mb-0">We've created some custom healer boxes using Bootstrap icons!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i class="fa-solid fa-code"></i></div>
+                                    <h2 class="fs-4 fw-bold">Simple clean code</h2>
+                                    <p class="mb-0">We keep our dependencies up to date and squash bugs as they come!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-xxl-4 mb-5">
+                            <div class="card bg-light border-0 h-100">
+                                <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                                    <div class="healer bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+                                        <i class="fa-solid fa-check"></i></div>
+                                    <h2 class="fs-4 fw-bold">A name you trust</h2>
+                                    <p class="mb-0">Start Bootstrap has been the leader in free Bootstrap templates since 2013!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
